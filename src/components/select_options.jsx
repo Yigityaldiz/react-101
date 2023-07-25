26ec50b1125387385dd19011de88368b59c84444
tdsrmd <demirsedatdemir@icloud.com>
2023-07-25 08:43:05 +0300

    Ufak tefek değişiklikler

--- src/components/select_options.jsx
+++ src/components/select_options.jsx
@@ -1,31 +1,12 @@
-
-
-
-  
-
-export default function Select({options}) {
-    
-    return (
-        <div>
-        <select>
-          {options.map((option) => (
-            <option value={option.label}>{option.label}</option>
-
-          ))
-          }
-         
-
-        </select>
-        
-      </div>
-      );
-
-      
-              
-} 
-
-    
-
-
-
-
+export default function Select({ options, label, ...props }) {
+  return (
+    <div className="grid grid-cols-2 gap-x-2">
+      <label htmlFor={props.id}>{label}:</label>
+      <select id={props.id}>
+        {options.map((option) => (
+          <option value={option.label}>{option.label}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
